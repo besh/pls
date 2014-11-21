4aae461002407c054103c01563b96884aa747d86
Dave Beshero <dbeshero@Daves-MBP-14.home>
2014-11-21 16:52:53 -0500

    added support for multiple requests

--- js/waitpls.js
+++ js/waitpls.js
@@ -1,13 +1,33 @@
 var waitpls = {
-  on: function(text) {
-    var c = document.querySelector('.waitpls');
+  var: cls      = '.waitpls',
+  var: waited   = ' js-has-waited',
+  var: overlays = [],
 
-    c.className = c.className + ' active';
+  on: function(text, parent) {
+    var c = document.querySelector('.waitpls.main');
+
+    if (parent) {
+      for(var i = 0, len = parent.length; i < len; i ++) {
+        if (parent[i].className.indexOf(waited) < 0) {
+          var html = c.outerHTML;
+
+          parent[i].insertAdjacentHTML('beforeend', html);
+          c = parent[i].querySelector(cls);
+          c.className = c.className.replace(/\b main\b/,'');
+          parent[i].className = parent[i].className + waited;
+        }
+        c = parent[i].querySelector(cls)
+      }
+    }
+
+    overlays.push(c);
     c.querySelector('.container p').innerHTML = text ? text : 'Waitpls';
+    c.className = c.className + ' active';
   },
 
   off: function(text, delay) {
-    var c = document.querySelector('.waitpls');
+    var c = overlays.shift();
+
     if (text) {
       c.querySelector('.container p').innerHTML = text;
     }
@@ -21,7 +41,13 @@ var waitpls = {
     }
 
     function _complete() {
-      c.className = c.className.replace(/\bactive\b/,'');
+      console.log(c)
+      _remove(c)
+    }
+
+    function _remove(elm) {
+
+      elm.className = elm.className.replace(/\b active\b/,'');
     }
   },
 }
